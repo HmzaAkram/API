b5e0e4a5d602514da132f891f6124e2a9404e436
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-22 00:54:49 +0500

    !

--- API/app/Models/Post.php
+++ API/app/Models/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
 }
