ed6443b599b17854aa5978b71fddc6249d6fbb12
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-22 01:04:38 +0500

    DONE

--- API/app/Http/Controllers/PostController.php
+++ API/app/Http/Controllers/PostController.php
@@ -1,10 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Post::all(), 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+
+        $post = Post::create($validated);
+        return response()->json($post, 201);
+    }
+
+    // Implement other methods (show, update, destroy) similarly
 }
--- API/routes/api.php
+++ API/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\studentcontroller;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,4 @@ return ["Name"=>"hmza","FNamza"=>"M.Akram"];
 );
 
 Route::get('/students',[studentcontroller::class,'list']);
+Route::apiResource('posts', PostController::class);
