4ef1d1854f9867947a71c1eaec1e330721406b06
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-22 00:52:29 +0500

    RESTFUL API

--- API/app/Http/Controllers/studentcontroller.php
+++ API/app/Http/Controllers/studentcontroller.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\students;
 class studentcontroller extends Controller
 {
     function list(){
-      return "Done";
+      return students::All();
     }
 }
