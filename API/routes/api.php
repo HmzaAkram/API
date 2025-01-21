1b936399f1f187a216d1dc3e8eb7bbb13e260c3e
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-22 00:28:54 +0500

    Update api.php

--- API/routes/api.php
+++ API/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\studentcontroller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,5 @@ Route::get('/test', function(){
 return ["Name"=>"hmza","FNamza"=>"M.Akram"];
 }
 );
+
+Route::get('/students',[studentcontroller::class,'list']);
